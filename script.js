92222a5631ce04ae8dede6669db6e5ee79b1cb0e
joao-erik2077 <joao.crisostomo01@etec.sp.gov.br>
2022-03-31 18:02:35 -0300

    Finalizando página inicial

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const oqueSaoDiv = document.getElementById('oque-sao');
+const exemplosDiv = document.getElementById('exemplos');
 const buttons = document.getElementById('button-list');
 
 function mostrarOqueSao() {
@@ -7,10 +8,12 @@ function mostrarOqueSao() {
 }
 
 function mostrarExemplos() {
-
+    exemplosDiv.style.display = 'initial';
+    buttons.style.display = 'none';
 }
 
 function voltar() {
     oqueSaoDiv.style.display = 'none';
+    exemplosDiv.style.display = 'none';
     buttons.style.display = 'flex';
 }
